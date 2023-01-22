fcd9bdc3f8f3e0fa10e04edfec683467d9824ce2
{} <1562487202@qq.com>
2023-01-22 15:51:58 +0800

    day07

---
+++ day07/springboot_03_read_data/src/main/java/com/itheima/springboot_03_read_data/Springboot03ReadDataApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_03_read_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03ReadDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03ReadDataApplication.class, args);
+    }
+
+}
